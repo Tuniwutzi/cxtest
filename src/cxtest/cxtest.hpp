2fe75c1bdfb7f2073f16b74855ed4555030a4dca
janb <jan.buenker@online.de>
2026-04-04 17:59:27 +0200

    Remove the shortened namespace and stick to cxtest

--- src/cxtest/cxtest.hpp
+++ src/cxtest/cxtest.hpp
@@ -370,7 +370,3 @@ void run_group(const Group& group, GroupOutputSink& sink) noexcept;
 void run_registered_tests(RunOutputSink& sink) noexcept;
 
 } // namespace cxtest
-
-#ifdef CXTEST_NAMESPACE_ABBREVIATION
-namespace CXTEST_NAMESPACE_ABBREVIATION = cxtest;
-#endif
